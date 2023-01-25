ba28f262397122f0860a9d816ce5221730ee968e
Nesha Mervin <nesha.mervin@gmail.com>
2023-01-24 23:37:31 -0500

    stored product's images' url in an array

---
+++ src/components/pages/sneakersImagesData.js
@@ -0,0 +1,8 @@
+export const sneakersImagesArr = [
+  '/assets/sneakers/ozweego-img1.webp',
+  '/assets/sneakers/ozweego-img2.webp',
+  '/assets/sneakers/ozweego-img3.webp',
+  '/assets/sneakers/ozweego-img4.webp',
+  '/assets/sneakers/ozweego-img5.webp',
+  '/assets/sneakers/ozweego-img6.webp',
+];
