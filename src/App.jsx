002d411bd3857a84ddf6c93832c9ce172f73ca15
Nesha Mervin <nesha.mervin@gmail.com>
2023-01-24 22:40:30 -0500

    used createPortal() to set up rendering mobile menu modal into a separate div

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,24 @@
-import { useState } from 'react';
+import { useContext } from 'react';
+import ReactDOM from 'react-dom';
+import { Context } from './context/Context';
+import MobileMenuModal from './components/navbars/MobileMenuModal';
 import './App.css';
+import Navbar from './components/navbars/Navbar';
 
 function App() {
-  return <div className="App"></div>;
+  const { toggleMenu } = useContext(Context);
+
+  return (
+    <div className="App">
+      <Navbar />
+
+      {toggleMenu &&
+        ReactDOM.createPortal(
+          <MobileMenuModal />,
+          document.getElementById('modal-root')
+        )}
+    </div>
+  );
 }
 
 export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,16 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter as Router } from 'react-router-dom';
+import { ContextProvider } from './context/Context';
+import App from './App';
+import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+  <Router>
+    <ContextProvider>
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>
+    </ContextProvider>
+  </Router>
+);
