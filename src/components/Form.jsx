d4a86a6e80ba5280293cc2f52f5fe213c4b62426
Nesha Mervin <nesha.mervin@gmail.com>
2023-01-26 01:51:39 -0500

    added styling to form component

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,12 +1,21 @@
 export default function Form() {
   return (
     <form className="flex flex-col items-center">
-      <div className="pb-6">
-        <button>-</button>
-        <input type="number" min="0" />
-        <button>+</button>
+      <div className="bg-gray-100 w-full h-14 flex justify-between items-center rounded-lg px-6">
+        <button className="text-orange-500 font-bold text-xl">-</button>
+        <input
+          className="bg-gray-100 text-center font-bold"
+          type="number"
+          min="0"
+        />
+        <button className="text-orange-500 font-bold text-xl">+</button>
       </div>
-      <button type="submit">Add to cart</button>
+      <button
+        className="bg-orange-500 font-display font-bold text-white w-full h-14 rounded-lg shadow-orange-200 shadow-lg mt-6"
+        type="submit"
+      >
+        Add to cart
+      </button>
     </form>
   );
 }
