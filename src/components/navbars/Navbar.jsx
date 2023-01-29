c24c213363d55188cbe2a39ba32dbad42ee3e2e8
Nesha Mervin <nesha.mervin@gmail.com>
2023-01-28 20:35:14 -0500

    set up navbar for tablets and desktops

--- src/components/navbars/Navbar.jsx
+++ src/components/navbars/Navbar.jsx
@@ -1,5 +1,7 @@
 import { useContext } from 'react';
+import { NavLink } from 'react-router-dom';
 import { Context } from '../../context/Context';
+import { navbarArr } from './navbarData';
 
 import hamburgerMenu from '/assets/icon-menu.svg';
 import logo from '/assets/logo.svg';
@@ -8,13 +10,23 @@ import iconCart from '/assets/icon-cart.svg';
 export default function Navbar() {
   const { openModal } = useContext(Context);
 
+  const navLinks = navbarArr.map(nav => (
+    <li key={nav.id}>
+      <NavLink to="/#">{nav.name}</NavLink>
+    </li>
+  ));
+
   return (
     <nav className="h-20 bg-white flex items-center px-6">
-      <button onClick={openModal} className="m-4">
+      <button onClick={openModal} className="m-4 md:hidden">
         <img src={hamburgerMenu} />
       </button>
 
-      <img src={logo} className="mr-auto" />
+      <img src={logo} className="mr-auto md:mr-14" />
+
+      <ul className="hidden font-display text-navGray block md:flex items-center gap-8 mr-auto">
+        {navLinks}
+      </ul>
 
       <button onClick={openModal}>
         <img src={iconCart} />
