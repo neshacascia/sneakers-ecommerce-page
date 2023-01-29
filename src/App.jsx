4c92e1356a75ee5e3ef20cd970a8ac4f283530c1
Nesha Mervin <nesha.mervin@gmail.com>
2023-01-29 18:15:01 -0500

    added modal for lightbox image gallery

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,11 @@ import ReactDOM from 'react-dom';
 import { Context } from './context/Context';
 import Home from './pages/Home';
 import MobileMenuModal from './components/navbars/MobileMenuModal';
+import ImageModal from './components/modals/ImageModal';
 import './App.css';
 
 function App() {
-  const { toggleMenu } = useContext(Context);
+  const { toggleMenu, imageGallery } = useContext(Context);
 
   return (
     <div className="App">
@@ -16,6 +17,11 @@ function App() {
         ReactDOM.createPortal(
           <MobileMenuModal />,
           document.getElementById('modal-root')
+        )}
+      {imageGallery &&
+        ReactDOM.createPortal(
+          <ImageModal />,
+          document.getElementById('image-modal-root')
         )}
     </div>
   );
---
+++ src/components/modals/ImageModal.jsx
@@ -0,0 +1,45 @@
+import { useContext } from 'react';
+import { Context } from '../../context/Context';
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faXmark } from '@fortawesome/free-solid-svg-icons';
+
+import closeBtn from '/assets/icon-close.svg';
+import prevIcon from '/assets/icon-previous.svg';
+import nextIcon from '/assets/icon-next.svg';
+import ImageThumbnails from '../details/ImageThumbnails';
+
+export default function ImageModal() {
+  const { sneakersImage, closeImageModal, prevImage, nextImage } =
+    useContext(Context);
+
+  return (
+    <div className="bg-overlay h-screen w-screen fixed z-10 flex flex-col justify-center items-center">
+      <button className="mb-6 ml-96" onClick={closeImageModal}>
+        <FontAwesomeIcon
+          icon={faXmark}
+          className="text-white text-xl hover:text-orange-500"
+        ></FontAwesomeIcon>
+      </button>
+
+      <div className="relative mb-4 :w-display">
+        <button
+          onClick={prevImage}
+          className="bg-white w-10 h-10 border-2 border-orange-300 rounded-full absolute top-1/2 -translate-y-1/2 flex items-center justify-center"
+        >
+          <img src={prevIcon} />
+        </button>
+        <img src={sneakersImage} className="h-imageModal rounded-2xl" />
+
+        <button
+          onClick={nextImage}
+          className="bg-white w-10 h-10 border-2 border-orange-300 rounded-full absolute top-1/2 right-0 -translate-y-1/2 flex items-center justify-center z-20"
+        >
+          <img src={nextIcon} />
+        </button>
+      </div>
+
+      <ImageThumbnails />
+    </div>
+  );
+}
