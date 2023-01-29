05cefc32505f105a72826759755cf65f97cb769e
Nesha Mervin <nesha.mervin@gmail.com>
2023-01-29 15:35:09 -0500

    set form buttons to sit beside one another on desktop

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -28,49 +28,60 @@ export default function Form() {
   }
 
   return (
-    <form className="flex flex-col items-center" onSubmit={submitHandler}>
-      <div className="bg-gray-100 w-full h-14 flex justify-between items-center rounded-lg px-6">
+    <>
+      <form
+        className="flex flex-col items-center h-auto xl:flex-row xl:gap-4"
+        onSubmit={submitHandler}
+      >
+        <div className="bg-gray-100 w-full h-14 flex justify-between items-center rounded-lg px-6 xl:w-quantity">
+          <button
+            className="text-orange-500 font-bold text-xl"
+            type="button"
+            onClick={minusQuantity}
+            disabled={disableMinusBtn}
+          >
+            -
+          </button>
+          <input
+            className="bg-gray-100 text-center font-bold"
+            type="number"
+            min="0"
+            max="10"
+            value={quantity}
+            disabled
+          />
+          <button
+            className="text-orange-500 font-bold text-xl"
+            type="button"
+            onClick={addQuantity}
+            disabled={disablePlusBtn}
+          >
+            +
+          </button>
+        </div>
+        {isAdded && !isTouched && quantity === 0 && (
+          <p className="text-red-500 font-display text-sm pt-6 xl:hidden">
+            Quantity needs to be greater than 0.
+          </p>
+        )}
+
         <button
-          className="text-orange-500 font-bold text-xl"
-          type="button"
-          onClick={minusQuantity}
-          disabled={disableMinusBtn}
+          className="bg-orange-500 font-display font-bold text-white w-full h-14 rounded-lg shadow-orange-200 shadow-lg mt-6 xl:mt-0"
+          type="submit"
         >
-          -
+          <FontAwesomeIcon
+            icon={faCartShopping}
+            className="pr-4"
+          ></FontAwesomeIcon>
+          Add to cart
         </button>
-        <input
-          className="bg-gray-100 text-center font-bold"
-          type="number"
-          min="0"
-          max="10"
-          value={quantity}
-          disabled
-        />
-        <button
-          className="text-orange-500 font-bold text-xl"
-          type="button"
-          onClick={addQuantity}
-          disabled={disablePlusBtn}
-        >
-          +
-        </button>
-      </div>
+      </form>
+
       {isAdded && !isTouched && quantity === 0 && (
-        <p className="text-red-500 font-display text-sm pt-6">
+        <p className="hidden xl:block text-red-500 font-display text-sm pt-6 text-center">
           Quantity needs to be greater than 0.
         </p>
       )}
-
-      <button
-        className="bg-orange-500 font-display font-bold text-white w-full h-14 rounded-lg shadow-orange-200 shadow-lg mt-6"
-        type="submit"
-      >
-        <FontAwesomeIcon
-          icon={faCartShopping}
-          className="pr-4"
-        ></FontAwesomeIcon>
-        Add to cart
-      </button>
-    </form>
+    </>
   );
 }
