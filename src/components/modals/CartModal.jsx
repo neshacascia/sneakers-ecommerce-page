dda13b1314461811cb1316205c4cf50d507d7bbb
Nesha Mervin <nesha.mervin@gmail.com>
2023-01-30 23:56:29 -0500

    added styling to cart modal

--- src/components/modals/CartModal.jsx
+++ src/components/modals/CartModal.jsx
@@ -1,9 +1,13 @@
 export default function CartModal() {
   return (
-    <div>
-      <span>Cart</span>
-      <hr />
-      <p>Your cart is empty.</p>
+    <div className="w-screen flex justify-center lg:justify-end">
+      <div className="bg-white font-display font-bold w-cart h-cart flex flex-col rounded-xl mt-24 shadow-cart fixed z-10 lg:mr-24">
+        <span className="px-6 my-6">Cart</span>
+        <hr />
+        <p className="text-center text-navGray px-6 pt-20">
+          Your cart is empty.
+        </p>
+      </div>
     </div>
   );
 }
