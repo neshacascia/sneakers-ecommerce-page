e957872457e22d6a429bd1dffd33ce9cd541c6a4
Nesha Mervin <nesha.mervin@gmail.com>
2023-02-01 14:33:38 -0500

    removed Router component and added CartProvider component

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter as Router } from 'react-router-dom';
 import { ContextProvider } from './context/Context';
+import CartProvider from './context/CartProvider';
 import App from './App';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <Router>
+  <CartProvider>
     <ContextProvider>
       <React.StrictMode>
         <App />
       </React.StrictMode>
     </ContextProvider>
-  </Router>
+  </CartProvider>
 );
