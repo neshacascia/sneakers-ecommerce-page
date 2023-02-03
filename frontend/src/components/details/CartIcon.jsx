09bc57de13ab78702c80e06f0d202912a0fae39e
Nesha Mervin <nesha.mervin@gmail.com>
2023-02-03 02:27:29 -0500

    disabled text selection

--- frontend/src/components/details/CartIcon.jsx
+++ frontend/src/components/details/CartIcon.jsx
@@ -8,7 +8,7 @@ export default function CartIcon() {
   const totalItems = cart.items.reduce((prev, curr) => prev + curr.quantity, 0);
 
   return (
-    <div className="w-cartIcon bg-orange-500 rounded-md z-10 absolute flex justify-center items-center left-4 bottom-4">
+    <div className="w-cartIcon bg-orange-500 rounded-md z-10 absolute flex justify-center items-center left-4 bottom-4 select-none">
       <span className="text-white text-xs text-center">{totalItems}</span>
     </div>
   );
